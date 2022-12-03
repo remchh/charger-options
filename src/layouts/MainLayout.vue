5054ab808d5149ba9d811488bceffa611b56138f
rafael mejia <rafaelm@rafaels-MacBook-Pro.local>
2022-12-02 23:01:58 -0600

    drawer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -60,13 +60,17 @@
     </q-page-container>
 
     <q-footer elevated class="bg-grey-8 text-white">
-      <q-toolbar>
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-          </q-avatar>
-        </q-toolbar-title>
-        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
+      <q-toolbar class="bg-primary text-white rounded-borders">
+        <q-btn round dense flat icon="menu" class="q-mr-xs" @click="toggleRightDrawer" />
+
+        <q-space />
+
+        <q-input dark dense standout v-model="text" input-class="text-right" class="q-ml-md">
+          <template v-slot:append>
+            <q-icon v-if="text === ''" name="search" />
+            <q-icon v-else name="clear" class="cursor-pointer" @click="text = ''" />
+          </template>
+        </q-input>
       </q-toolbar>
     </q-footer>
 
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,39 +1,42 @@
 <template>
   <q-page class="flex flex-center q-pa-md">
+    <div>
+      <h3>Look for the best charger for your Phone!</h3>
+    </div>
     <q-card class="my-card">
-      <img src="https://cdn.quasar.dev/img/parallax2.jpg">
+      <img src="https://images.unsplash.com/photo-1530319067432-f2a729c03db5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2889&q=80">
 
       <q-list>
         <q-item clickable>
           <q-item-section avatar>
-            <q-icon color="primary" name="local_bar" />
+            <q-icon color="primary" name="smartphone" />
           </q-item-section>
 
           <q-item-section>
-            <q-item-label>Bar XYZ</q-item-label>
-            <q-item-label caption>Have a drink.</q-item-label>
+            <q-item-label>iPhone 13</q-item-label>
+            <q-item-label caption>Phone model</q-item-label>
           </q-item-section>
         </q-item>
 
         <q-item clickable>
           <q-item-section avatar>
-            <q-icon color="red" name="local_gas_station" />
+            <q-icon color="red" name="power" />
           </q-item-section>
 
           <q-item-section>
-            <q-item-label>Gas Station</q-item-label>
-            <q-item-label caption>Fill your gas tank.</q-item-label>
+            <q-item-label>20W</q-item-label>
+            <q-item-label caption>Max wired watts supported</q-item-label>
           </q-item-section>
         </q-item>
 
         <q-item clickable>
           <q-item-section avatar>
-            <q-icon color="amber" name="local_movies" />
+            <q-icon color="amber" name="wifi" />
           </q-item-section>
 
           <q-item-section>
-            <q-item-label>Cinema XYZ</q-item-label>
-            <q-item-label caption>Watch a movie.</q-item-label>
+            <q-item-label>15W</q-item-label>
+            <q-item-label caption>Max wireless watts supported</q-item-label>
           </q-item-section>
         </q-item>
       </q-list>
