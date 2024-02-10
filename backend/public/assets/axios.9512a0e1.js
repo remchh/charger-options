e41252e05345f28713195d84ec9921f9a1bc769d
rafael mejia <rafaelm@rafaels-MacBook-Pro.local>
2024-02-10 16:48:46 -0600

    deploy backend on render

---
+++ backend/public/assets/axios.9512a0e1.js
@@ -0,0 +1,1 @@
+import{b as i}from"./index.8dafa657.js";import{a}from"./axios.6b484fa5.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
